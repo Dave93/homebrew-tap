003a05fd447127f3ddfd70956343417c4b52d773
Dave93 <yul.davron.93@gmail.com>
2026-03-05 10:30:35 +0500

    Update velos to 0.1.0

--- Formula/velos.rb
+++ Formula/velos.rb
@@ -7,22 +7,22 @@ class Velos < Formula
   on_macos do
     on_arm do
       url "https://github.com/Dave93/velos/releases/download/v#{version}/velos-macos-arm64.tar.gz"
-      sha256 "23b0fcf02e14c826644e7b21b1fdf84ba2973a731ec8a67dda97e21c23ec6678"
+      sha256 "2631cbf646c5e7eec30863784f4a14cb61c0d91cb3bbd4393b773c21e7899ff1"
     end
     on_intel do
       url "https://github.com/Dave93/velos/releases/download/v#{version}/velos-macos-x86_64.tar.gz"
-      sha256 "49ea1ce8a84fa6e2cbc3fdeba72badbd24145f5a5693f76445b5225e7fcc6d2a"
+      sha256 "8bb529235793b3349628798955cc43bb4a7455213396644465968dd855040e07"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Dave93/velos/releases/download/v#{version}/velos-linux-arm64.tar.gz"
-      sha256 "a5a0c4e0ceab293d276958f5b5506f00e99ddb51bba07a56cff6d0a2201ff572"
+      sha256 "836047c6e070a380a563eb7a7b4a1d1b5effc555c4bc5167c7798640c6ee9820"
     end
     on_intel do
       url "https://github.com/Dave93/velos/releases/download/v#{version}/velos-linux-x86_64.tar.gz"
-      sha256 "155f4a5333f1a30a06682cbd0892e5d2ce8aa13ca6e805bd1abfc015bc812cac"
+      sha256 "f6fec60d52cbb4bd5e3c6e81a83b83b9dfdcfae174c0ab08c3d7f4526c781055"
     end
   end
 
