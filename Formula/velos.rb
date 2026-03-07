25bbe7d2a7cf884ba568c700720f4e18c5f8b08a
Dave93 <yul.davron.93@gmail.com>
2026-03-07 12:58:22 +0500

    Update velos to 0.1.13

--- Formula/velos.rb
+++ Formula/velos.rb
@@ -1,28 +1,28 @@
 class Velos < Formula
   desc "High-performance AI-friendly process manager"
   homepage "https://github.com/Dave93/velos"
-  version "0.1.12"
+  version "0.1.13"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/Dave93/velos/releases/download/v#{version}/velos-macos-arm64.tar.gz"
-      sha256 "30eaf6a11ae3912efd2ecaaee37263e1d294969f64956fbc01adb63da8083157"
+      sha256 "fdd90d6e90ba7a86603e3d1b3c3ced85d6a925b9aa1571856a3b9c2ea394330e"
     end
     on_intel do
       url "https://github.com/Dave93/velos/releases/download/v#{version}/velos-macos-x86_64.tar.gz"
-      sha256 "c8f6c0ef82ad264e32c25fe6fb022fd409de23822374d7cd23e99c047f7b97c7"
+      sha256 "de2f90733eaedb19ff20e6103e4e9ad4140fc32ca3483b42da26efee3a5d257a"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Dave93/velos/releases/download/v#{version}/velos-linux-arm64.tar.gz"
-      sha256 "9a591ccf4518e1fe3d8e40fb213648b99e3c295f6ee9757c5e2216b7af220e1c"
+      sha256 "69946bece3620ead26894b22cc2696040db9bda4ec6e9ad31d7d0b11bf151be2"
     end
     on_intel do
       url "https://github.com/Dave93/velos/releases/download/v#{version}/velos-linux-x86_64.tar.gz"
-      sha256 "efdee6f6ea2b6032169a4f0b00d5b6a9ab9fa4979ce6df01eae1757d1816c98f"
+      sha256 "3b4df032fd4db04fbf35e60898f64120d6abd65e999f79cf903a70acb9a85ec0"
     end
   end
 
